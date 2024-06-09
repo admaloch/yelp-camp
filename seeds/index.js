d1239bea2d877b18b8c9b26dc99dc9ef163f62e5
Andrew Maloch <admaloch91@gmail.com>
2024-06-09 15:26:32 -0400

    server breaking now.. revert

--- seeds/index.js
+++ seeds/index.js
@@ -5,17 +5,9 @@ const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 const fetch = require('node-fetch');
 
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
-const unsplashToken = process.env.UNSPLASH_TOKEN
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 
@@ -24,20 +16,24 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+
+
+
 const seedDB = async () => {
     // await Campground.deleteMany({});
     for (let campground of campgroundData) {
         const price = Math.floor(Math.random() * 20) + 10;
         const searchTerm = campground.keywords.join(',');
+        const accessKey = '_2Nq_YNn7y7gbcweITcuLKyol_Zh49mw3J6QyZ2OwT0'; // Replace 'your_access_key' with your actual access key
         const count = 5; // Specify the number of images you want to retrieve
-        const url = `https://api.unsplash.com/photos/random?client_id=${unsplashToken}&query=${searchTerm}&count=${count}`;
+        const url = `https://api.unsplash.com/photos/random?client_id=${accessKey}&query=${searchTerm}&count=${count}`;
         const response = await fetch(url);
         const data = await response.json();
         const images = data.map(item => {
             return { url: item.urls.small, filename: `YelpCamp/${item.id}` }
         })
         const camp = new Campground({
-            author: '6665f89bd86d080043eba2cd',
+            author: '6646109e1c2497363d52ef89',
             location: `${campground.city}, ${campground.location}`,
             title: campground.name,
             description: campground.description,
@@ -58,5 +54,14 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
-// console.log(campgroundData.length)
 
+// console.log(campgroundData[0].keywords.join(','))
+
+// const searchKeys = campgroundData[0].keywords.join(',');
+// const searchTerm = `${campgroundData[0].city},${searchKeys}`
+// console.log(searchTerm)
+
+console.log(campgroundData.length)
+
+
+// console.log(campgroundData.length)
